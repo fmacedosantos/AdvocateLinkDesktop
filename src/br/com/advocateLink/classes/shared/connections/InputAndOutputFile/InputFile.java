1dec2f827b11a296ade2c29594c6924c90e36e37
01nevida <01@vida>
2023-09-28 09:48:59 -0300

    preparo para o terceiro Sprint

---
+++ src/br/com/advocateLink/classes/shared/connections/InputAndOutputFile/InputFile.java
@@ -0,0 +1,30 @@
+package br.com.advocateLink.classes.shared.connections.InputAndOutputFile;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class InputFile {
+    //Iniciar conexao OFFLine no 3 Sprint....
+    //Iniciar conexao OFFLine no 3 Sprint....
+    //Iniciar conexao OFFLine no 3 Sprint....
+    //Iniciar conexao OFFLine no 3 Sprint....
+    //Iniciar conexao OFFLine no 3 Sprint....
+    //Iniciar conexao OFFLine no 3 Sprint....
+
+//    private FileReader fileReader=new FileReader("EmployeeFile.txt");
+//    private BufferedReader bufferedReader = new BufferedReader(fileReader);
+//
+//    public InputFile() throws FileNotFoundException {
+//
+//    }
+//
+//    public Boolean readFile() throws IOException {
+//        while (bufferedReader!=null){
+//            System.out.println(bufferedReader.readLine());
+//        }
+//        return false;
+//    }
+}
