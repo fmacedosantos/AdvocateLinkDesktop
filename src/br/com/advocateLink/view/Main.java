d41de9f593d9c433a9f27e8983f6e4a7fd3cea7c
01nevida <01@vida>
2023-10-28 09:56:25 -0300

    tratamento de perca de conexao com BD

--- src/br/com/advocateLink/view/Main.java
+++ src/br/com/advocateLink/view/Main.java
@@ -1,26 +1,23 @@
 package br.com.advocateLink.view;
+import br.com.advocateLink.classes.shared.connections.database.ConnectionDataBase;
 import br.com.advocateLink.classes.shared.connections.https.HttpsConnections;
 import br.com.advocateLink.view.screens.Login;
 import javax.swing.*;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main {
-    public enum AppConnectState {
-        ON,
-        OFF;
-    }
-    public static AppConnectState currentConnectState;
-    public static void main(String[] args) throws IOException, InterruptedException {
+    private static ConnectionDataBase dataBase = new ConnectionDataBase();
+    public static void main(String[] args) throws SQLException {
         Login login = new Login("AdvocateLink");
         login.setVisible(true);
         try {
-            HttpsConnections.getHttps();// inicia a conexao com a API do Spring
-            currentConnectState=AppConnectState.ON;
-        }catch (ExceptionInInitializerError ex){
-            JOptionPane.showMessageDialog(null,"VOCE ESTA INICIANDO O APP NO MODO OFF");
-            JOptionPane.showMessageDialog(null,"Falha ao tentar acessar a API ADVOCATE-Link,Por favor, verifique se a conexao com a API ADVOCATE-Link esta sicronizada ou iniciada, para mais ajuda acesse: "+ "https://github.com/guilhermevini2013/Advocatelink-APPDesktop-consumindo-API");
-            currentConnectState=AppConnectState.OFF;
-        }finally {
+            dataBase.connectionDB();
+        }catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,"VOCE ESTA SEM CONEXAO COM BD, TENTE MAIS TARDE");
+            System.exit(0);
+        } finally {
+            dataBase.closeDB();
             JOptionPane.showMessageDialog(null,"OLA usuario, para que voce nao fique 1 hora procurando a senha para entrar no sistema, tome aqui: login: admin  senha: admin  , Muito Obrigado :)");
         }
     }
